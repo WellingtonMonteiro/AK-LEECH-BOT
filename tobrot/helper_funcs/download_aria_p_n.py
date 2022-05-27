07c85b157b48be91519716fbd54cef1907fd97c4
akprivatebots <90455659+akprivatebots@users.noreply.github.com>
2022-05-27 15:50:48 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -288,7 +288,7 @@ async def call_apropriate_function(
                         f"<b><a href='tg://user?id={user_id}'>📁 Your Requested Files</a></b>\n\n"
                     )
                     message_to_send = mention_req_user + message_to_send
-                    message_to_send = message_to_send + "\n\n" + "#uploaded\n\n<b>💥 Powered By : @MovieFinaticss</b>"
+                    message_to_send = message_to_send + "\n\n" + "#uploaded\n\n<b>💥 Powered By : @AKLEECHZONE</b>"
                 else:
                     message_to_send = "<i>FAILED</i> to upload files. 😞"
                 await user_message.reply_text(
